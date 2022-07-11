2d2afadaab485c0eeec53e7fdbe1b566f3dac2d7
Alexandru Seman <alexandru.seman@tremend.com>
2022-07-12 02:06:59 +0300

    Downgrade PHP version to 7.4

--- tests/Smoke/V1/ReadActionTest.php
+++ tests/Smoke/V1/ReadActionTest.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Smoke\V1;
 
-use App\Http\V1\Action\AbstractAction;
 use App\Http\V1\Action\Post\Read\ReadAction;
-//use App\Tests\Smoke\AbstractActionTest;
+use App\Tests\Smoke\AbstractActionTest;
 use App\Tests\Smoke\Traits\PostCreatorTrait;
 use App\Tests\Smoke\Traits\UserCreatorTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -26,71 +25,23 @@ class ReadActionTest extends KernelTestCase
 
     protected function setUp(): void
     {
-//        $kernel = self::bootKernel();
-//
-//        $this->entityManager = $kernel->getContainer()
-//            ->get('doctrine')
-//            ->getManager();
-//
-//        $this->action = $kernel->getContainer()->get(ReadAction::class);
-//
-//        $this->entityManager->getConnection()->ensureConnectedToPrimary();
-//        $this->entityManager->beginTransaction();
-//        $this->entityManager->getConnection()->setAutoCommit(false);
+        $kernel = self::bootKernel();
 
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
 
-//        $this->init(ReadAction::class);
-
-        static::bootKernel();
-
-//        $action   = static::$kernel->getContainer()->get(ReadAction::class);
-
-//        if (empty($action)) {
-//            throw new \Exception(sprintf('Container failed to resolve class: %s', ReadAction::class));
-//        }
-
-//        $this->action        = $action;
-        $this->entityManager = static::$kernel->getContainer()->get('doctrine.orm.entity_manager');
-
-
-//        $this->action = static::$kernel->getContainer()->get(ReadAction::class);
-        $this->action = static::$kernel->getContainer()->get(ReadAction::class);
-//        $this->entityManager = static::$kernel
-//            ->getContainer()
-//            ->get('doctrine')
-//            ->getManager();
-
-
-//        var_dump(get_class($this->entityManager));
-//        var_dump(get_class($this->action));
-//        die();
+        $this->action = $kernel->getContainer()->get(ReadAction::class);
 
         $this->entityManager->getConnection()->ensureConnectedToPrimary();
         $this->entityManager->beginTransaction();
         $this->entityManager->getConnection()->setAutoCommit(false);
     }
 
-
-//    protected static function bootKernel(array $options = [])
-//    {
-//        static::ensureKernelShutdown();
-//
-//        static::$kernel = static::createKernel($options);
-//        static::$kernel->boot();
-//        static::$booted = true;
-//
-//        $container = static::$kernel->getContainer();
-//
-//        var_dump(get_class($container));
-//        static::$container = $container->has('test.service_container') ? $container->get('test.service_container') : $container;
-//
-//        return static::$kernel;
-//    }
-
     public function testSuccess()
     {
         $userId = 25;
-        $user = $this->createUser($userId);
+        $user   = $this->createUser($userId);
         $this->entityManager->persist($user);
         $post = $this->createPost('Post title', 'Post body', $user);
         $this->entityManager->persist($post);
